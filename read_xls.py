4d2cee92aa7f8a939af9e657e9f010462d0c7714
Jeremiah Richter <jeremiahrichter@gmail.com>
2015-12-14 16:25:58 -0500

    Add everything to function main() in read_xls.py, change variable names to be pythonic

--- read_xls.py
+++ read_xls.py
@@ -7,56 +7,56 @@ import openpyxl as op
 from string_functions import display_menu
 from string_functions import display_rows
 
-#   if len(sys.argv) < 2:
-if False:
-    print('Please specify a spreadsheet file to open.')
-else:
-    try:
-        # wb = op.load_workbook(sys.argv[1])
-        wb = op.load_workbook('test.xlsx')  # load a workbook using openpyxl
 
-        # print('Opened file %s' % wb.name)
+def main():
+    if len(sys.argv) < 2:
+        print('Please enter a filename')
+        exit(1)
+    else:
+        try:
+            wb = op.load_workbook(sys.argv[1])
 
-        if wb is not None:
-            exitChosen = False
-            row_start = 1
-            row_end = 5
-            ws_num = 0
-            col_start = 1
-            col_end = 5
+            if wb is not None:
+                exit_chosen = False
+                row_start = 1
+                row_end = 5
+                ws_num = 0
+                col_start = 1
+                col_end = 5
 
-            display_rows(wb, ws_num, row_start, row_end, col_start, col_end)
+                display_rows(wb, ws_num, row_start, row_end, col_start, col_end)
 
-            while not exitChosen:
+                while not exit_chosen:
 
-                choice = input('choice: ')
-                choice = choice if len(choice) == 1 else choice[0]
+                    choice = input('choice: ')
+                    choice = choice if len(choice) == 1 else choice[0]
 
-                if choice == 'n':
-                    ws_num += 1
-                elif choice == 'r':
-                    col_start += 5
-                    col_end += 5
-                elif choice == 'l' and col_end > 5:
-                    col_start -= 5
-                    col_end -= 5
-                elif choice == 'p' and ws_num > 0:
-                    ws_num -= 1
-                elif choice == 'd':
-                    row_start += 5
-                    row_end += 5
-                elif choice == 'u' and row_end > 5:
-                    row_start -= 5
-                    row_end -= 5
-                elif choice == 'x':
-                    exitChosen = True
-                else:
-                    print('invalid choice, please choose one of the options provided\n')
-                    display_menu(wb, ws_num, row_start, row_end, col_start, col_end)
-                    continue
-                if not exitChosen:
-                    display_rows(wb, ws_num, row_start, row_end, col_start, col_end)
+                    if choice == 'n':
+                        ws_num += 1
+                    elif choice == 'r':
+                        col_start += 5
+                        col_end += 5
+                    elif choice == 'l' and col_end > 5:
+                        col_start -= 5
+                        col_end -= 5
+                    elif choice == 'p' and ws_num > 0:
+                        ws_num -= 1
+                    elif choice == 'd':
+                        row_start += 5
+                        row_end += 5
+                    elif choice == 'u' and row_end > 5:
+                        row_start -= 5
+                        row_end -= 5
+                    elif choice == 'x':
+                        exit_chosen = True
+                    else:
+                        print('invalid choice, please choose one of the options provided\n')
+                        display_menu(wb, ws_num, row_start, row_end, col_start, col_end)
+                        continue
+                    if not exit_chosen:
+                        display_rows(wb, ws_num, row_start, row_end, col_start, col_end)
 
-            sys.exit(0)
-    except Exception as e:
-        sys.exit(1)
+                sys.exit(0)
+        except (NameError, TypeError) as e:
+            print(str(e))
+            exit(1)
