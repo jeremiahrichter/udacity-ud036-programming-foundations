55331f0ccc7e09f6e6af1fa4f783e224c1003d02
Jeremiah Richter <jeremiahrichter@gmail.com>
2015-12-14 16:08:03 -0500

    Reformat code with Pycharm

--- read_xls.py
+++ read_xls.py
@@ -1,61 +1,62 @@
 #!/usr/bin/python
 
 import sys
+
 import openpyxl as op
+
 from string_functions import display_menu
 from string_functions import display_rows
 
 #   if len(sys.argv) < 2:
 if False:
-  print('Please specify a spreadsheet file to open.')
+    print('Please specify a spreadsheet file to open.')
 else:
-  try:
-#     wb = op.load_workbook(sys.argv[1])
-    wb = op.load_workbook('test.xlsx') # load a workbook using openpyxl
+    try:
+        # wb = op.load_workbook(sys.argv[1])
+        wb = op.load_workbook('test.xlsx')  # load a workbook using openpyxl
 
-##    print('Opened file %s' % wb.name)
+        # print('Opened file %s' % wb.name)
 
-    
-    if wb is not None:
-      exit = False
-      row_start = 1
-      row_end = 5
-      ws_num = 0
-      col_start = 1
-      col_end = 5
+        if wb is not None:
+            exit = False
+            row_start = 1
+            row_end = 5
+            ws_num = 0
+            col_start = 1
+            col_end = 5
 
-      display_rows(wb, ws_num, row_start, row_end, col_start, col_end)
-      
-      while not exit:
-        
-        choice = input('choice: ')
-        choice = choice if len(choice) == 1 else choice [0]
+            display_rows(wb, ws_num, row_start, row_end, col_start, col_end)
 
-        if choice == 'n':
-          ws_num += 1
-        elif choice == 'r':
-          col_start += 5
-          col_end += 5
-        elif choice == 'l' and col_end > 5:
-          col_start -= 5
-          col_end -= 5
-        elif choice == 'p' and ws_num > 0:
-          ws_num -= 1
-        elif choice == 'd':
-          row_start += 5
-          row_end += 5
-        elif choice == 'u' and row_end > 5:
-          row_start -= 5
-          row_end -= 5
-        elif choice == 'x':
-          exit = True
-        else:
-          print('invalid choice, please choose one of the options provided\n')
-          display_menu(wb, ws_num, row_start, row_end, col_start, col_end)
-          continue
-        if not exit:
-          display_rows(wb, ws_num, row_start, row_end, col_start, col_end)
+            while not exit:
 
-      sys.exit(0)
-  except:
-    sys.exit(1)
+                choice = input('choice: ')
+                choice = choice if len(choice) == 1 else choice[0]
+
+                if choice == 'n':
+                    ws_num += 1
+                elif choice == 'r':
+                    col_start += 5
+                    col_end += 5
+                elif choice == 'l' and col_end > 5:
+                    col_start -= 5
+                    col_end -= 5
+                elif choice == 'p' and ws_num > 0:
+                    ws_num -= 1
+                elif choice == 'd':
+                    row_start += 5
+                    row_end += 5
+                elif choice == 'u' and row_end > 5:
+                    row_start -= 5
+                    row_end -= 5
+                elif choice == 'x':
+                    exit = True
+                else:
+                    print('invalid choice, please choose one of the options provided\n')
+                    display_menu(wb, ws_num, row_start, row_end, col_start, col_end)
+                    continue
+                if not exit:
+                    display_rows(wb, ws_num, row_start, row_end, col_start, col_end)
+
+            sys.exit(0)
+    except:
+        sys.exit(1)
--- string_functions.py
+++ string_functions.py
@@ -17,60 +17,64 @@ LEFT_CHOICE = '\n(l): display last 5 columns to the left\n'
 PREVIOUS_CHOICE = '\n(p): previous worksheet\n'
 UP_CHOICE = '\n(u): display last 5 rows upwards\n'
 
-def check_rows(row_start, row_end):
-  row_start = row_start if row_start >= 1 else 1
-  row_end = row_end if ((row_end-row_start) == 4) else row_start + 4 # ensure range of 5
 
-  return row_start, row_end
+def check_rows(row_start, row_end):
+    row_start = row_start if row_start >= 1 else 1
+    row_end = row_end if ((row_end - row_start) == 4) else row_start + 4  # ensure range of 5
+
+    return row_start, row_end
+
 
 def check_cols(col_start, col_end):
-  col_start = col_start if col_start >= 1 else 1
-  col_end = col_end if ((col_end - col_start) == 4) else col_start + 4 # ensure range of 5
+    col_start = col_start if col_start >= 1 else 1
+    col_end = col_end if ((col_end - col_start) == 4) else col_start + 4  # ensure range of 5
 
-  return col_start, col_end
+    return col_start, col_end
+
 
 def check_worksheet(wb, ws_num):
-  if wb is None: # ensure we have a valid workbook object
-    print('Workbook not valid')
-    wb = op.workbook.WorkBook()
+    if wb is None:  # ensure we have a valid workbook object
+        print('Workbook not valid')
+        wb = op.workbook.WorkBook()
 
-  ws_num = ws_num if not ws_num < 0 else 0 # check range on ws_num
-  ws_num = ws_num if ws_num < len(wb.worksheets) else len(wb.worksheets) - 1 # check range on ws_num
+    ws_num = ws_num if not ws_num < 0 else 0  # check range on ws_num
+    ws_num = ws_num if ws_num < len(wb.worksheets) else len(wb.worksheets) - 1  # check range on ws_num
 
-  return wb, ws_num
+    return wb, ws_num
+
 
 def display_menu(wb, ws_num, row_start=1, row_end=5, col_start=1, col_end=5):
-  '''Display the main menu of options'''
+    '''Display the main menu of options'''
 
-  wb, ws_num = check_worksheet(wb, ws_num)
-  row_start, row_end = check_rows(row_start, row_end)
-  col_start, col_end = check_cols(col_start, col_end)
+    wb, ws_num = check_worksheet(wb, ws_num)
+    row_start, row_end = check_rows(row_start, row_end)
+    col_start, col_end = check_cols(col_start, col_end)
 
-  previous = PREVIOUS_CHOICE if ws_num > 0 else "" # display choice only if valid
-  left = LEFT_CHOICE if col_end > 5 else "" # display choice only if valid
-  up = UP_CHOICE if row_end > 5 else "" # display choice only if valid
+    previous = PREVIOUS_CHOICE if ws_num > 0 else ""  # display choice only if valid
+    left = LEFT_CHOICE if col_end > 5 else ""  # display choice only if valid
+    up = UP_CHOICE if row_end > 5 else ""  # display choice only if valid
 
-  print(CHOICES.format(row_start, row_end, col_start, col_end, previous, left, up))
+    print(CHOICES.format(row_start, row_end, col_start, col_end, previous, left, up))
 
 
-def display_rows(wb, ws_num, row_start=1, row_end=5, col_start=1,col_end=5):
-  '''display the specified rows/columns of the spreadsheet'''
+def display_rows(wb, ws_num, row_start=1, row_end=5, col_start=1, col_end=5):
+    '''display the specified rows/columns of the spreadsheet'''
 
-  wb, ws_num = check_worksheet(wb, ws_num)
-  row_start, row_end = check_rows(row_start, row_end)
-  col_start, col_end = check_cols(col_start, col_end)
-  row_nums = [i for i in range(row_start, row_end +1)] # enumerate range of 5 row numbers
-  col_letters = [get_column_letter(i) for i in range(col_start, col_end +1)] # get 5 column letters
+    wb, ws_num = check_worksheet(wb, ws_num)
+    row_start, row_end = check_rows(row_start, row_end)
+    col_start, col_end = check_cols(col_start, col_end)
+    row_nums = [i for i in range(row_start, row_end + 1)]  # enumerate range of 5 row numbers
+    col_letters = [get_column_letter(i) for i in range(col_start, col_end + 1)]  # get 5 column letters
 
-  print(ROW_5.format("", *col_letters))
-  print(SEPARATOR)
-  ws = wb.worksheets[ws_num]
-
-  for row in row_nums: # truncates and prints up to row_end, col_end number of cell values
-    value_list = [str(ws.cell(row=row, column=col).value)
-                          if ws.cell(row=row, column=col).value is not None else ""
-                          for col in range(col_start, col_end +1)]
-    short_values = [value if len(value) <= 10 else value[:7] + '...' for value in value_list]
-    print(ROW_5.format(row, *short_values))
+    print(ROW_5.format("", *col_letters))
     print(SEPARATOR)
-  display_menu(wb, ws_num, row_start, row_end, col_start, col_end)
+    ws = wb.worksheets[ws_num]
+
+    for row in row_nums:  # truncates and prints up to row_end, col_end number of cell values
+        value_list = [str(ws.cell(row=row, column=col).value)
+                      if ws.cell(row=row, column=col).value is not None else ""
+                      for col in range(col_start, col_end + 1)]
+        short_values = [value if len(value) <= 10 else value[:7] + '...' for value in value_list]
+        print(ROW_5.format(row, *short_values))
+        print(SEPARATOR)
+    display_menu(wb, ws_num, row_start, row_end, col_start, col_end)
